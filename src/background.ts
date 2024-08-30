6a930a1b26b6aa2129755210dfacaa0b6184ab4c
SamuvelRaja <josephsamuvel001@gmail.com>
2024-08-30 22:35:28 +0530

    added code to hide videos apart from the whitelist

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,7 @@
-const ythome = 'https://www.youtube.com';
-const ytwatch='https://www.youtube.com/watch'
-const ytresults='https://www.youtube.com/results'
 
-const exData={
-  whitelist:['@Mrwhosetheboss','@A2DChannel']
-}
 
-chrome.action.onClicked.addListener(async (tab:chrome.tabs.Tab) => {
-  if (tab?.url?.startsWith(ythome)) {
-    const nodeList = document.querySelectorAll(".ytd-channel-name.complex-string>a");
-    const feedList: HTMLAnchorElement[] = Array.from(nodeList) as HTMLAnchorElement[];
-    feedList.forEach((node:HTMLAnchorElement)=>{
-      console.log(node)
-      if(!exData.whitelist.includes(node.href)){
-        console.log(node.parentElement,node.parentElement?.closest(".ytd-rich-grid-renderer"))
-      }
-    })
-  }
-  if(tab?.url?.startsWith(ytwatch)){
-
-  }if(tab?.url?.startsWith(ytresults)){
-
-  }
+chrome.action.onClicked.addListener(async () => {
+  console.log("fired")
+ 
   
 });
--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,34 @@
 console.log('Content script loaded');
+const ythome = 'https://www.youtube.com';
+const ytwatch='https://www.youtube.com/watch'
+const ytresults='https://www.youtube.com/results'
+const currentURL=window.location.href
+console.log(currentURL,"cl")
+const exData={
+  whitelist:['https://www.youtube.com/@Mrwhosetheboss','https://www.youtube.com/@A2DChannel']
+}
+function filterVideos(){
+    if (currentURL?.startsWith(ythome)) {
+        const nodeList = document.querySelectorAll(".ytd-channel-name.complex-string>a");
+        const feedList: HTMLAnchorElement[] = Array.from(nodeList) as HTMLAnchorElement[];
+        feedList.forEach((node:HTMLAnchorElement)=>{
+          console.log(node,node.href,!exData.whitelist.includes(node.href),"am")
+          if(!exData.whitelist.includes(node.href)){
+            console.log(node.parentElement,node.closest(".ytd-rich-grid-renderer"),"rm")
+            const vidContainer:HTMLDivElement=node.closest(".ytd-rich-grid-renderer")!
+            vidContainer.style.display="none"
+          }
+        })
+      }
+      if(currentURL?.startsWith(ytwatch)){
+
+      }if(currentURL?.startsWith(ytresults)){
+
+      }
+}
+  // YouTube homepage dynamically loads content, so we need to monitor the DOM
+const observer = new MutationObserver(filterVideos);
+observer.observe(document.body, { childList: true, subtree: true });
+
+// Run filtering immediately on page load
+filterVideos();
